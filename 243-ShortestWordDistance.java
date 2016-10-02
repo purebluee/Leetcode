57b0d48ff2d984f7826919ac5e3abdcc01f94f32
C <cong@1x-nat-vl931-172-30-156-245.wireless.umass.edu>
2016-10-02 16:00:15 -0400

    updated 243

--- 243-ShortestWordDistance.java
+++ 243-ShortestWordDistance.java
@@ -1,4 +1,20 @@
 public class Solution {
+    //brute force, O(n^2)
+    public int shortestDistanceBF(String[] words, String word1, String word2) {
+        int minDistance = words.length;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                for (int j = 0; j < words.length; j++) {
+                    if (words[j].equals(word2)) {
+                        minDistance = Math.min(minDistance, Math.abs(i - j));
+                    }
+                }
+            }
+        }
+        return minDistance;
+    }
+
+    //two pointers, one pass O(n)
     public int shortestDistance(String[] words, String word1, String word2) {
         if (words.length == 0 || words == null)
             return 0;
