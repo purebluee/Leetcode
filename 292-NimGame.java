f3d13d2446688250e66db29fbde547a7b7db6a51
Cong <c.wang049@gmail.com>
2016-10-01 20:53:57 -0400

    add second sol Nim game

--- 292-NimGame.java
+++ 292-NimGame.java
@@ -4,4 +4,20 @@ public class Solution {
         if (n % 4 == 0) return false;
         else return true;
     }
+    
+    public boolean canWinNimDP(int n) {
+        if (n < 0)
+            return false;
+        if (n < 4 && n >= 0)
+            return true;
+        boolean[] res = new boolean[n+1];
+        res[0] = true;
+        res[1] = true;
+        res[2] = true;
+        res[3] = true;
+        for (int i = 4; i <= n; i++){
+            res[i] = !(res[i - 1] && res[i - 2] && res[i - 3]);
+        }
+        return res[n];
+    }
 }
