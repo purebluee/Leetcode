edbf0f43c911726336cb43569a83b04b2e9623a6
Cong <c.wang049@gmail.com>
2016-10-01 21:26:38 -0400

    add sol to Q242

--- 242-ValidAnagram.java
+++ 242-ValidAnagram.java
@@ -11,4 +11,23 @@ public class Solution {
         t = new String(chart);
         return s.equals(t);
     }
+
+    public boolean isAnagramSol2(String s, String t) {
+        if (s.length() != t.length()){
+            return false;
+        }
+        int[] count = new int['z' - 'a' + 1];
+        for (char c : s.toCharArray()){
+            count[c - 'a']++;
+        }
+        for (char d : t.toCharArray()){
+            count[d - 'a']--;
+        }
+        for (int i = 0; i < count.length; i++){
+            if (count[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }    
 } 
