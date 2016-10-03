7a859ff65719ca69bb9fab4884175d33459f2a9b
C <cong@1x-nat-vl931-172-30-156-245.wireless.umass.edu>
2016-10-02 22:05:13 -0400

    updated Q27

--- 27-RemoveElement.java
+++ 27-RemoveElement.java
@@ -32,4 +32,21 @@ public class Solution {
         System.out.println(start);
         return start;
     }
+
+    //Two Pointers - when elements to remove are rare
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                // reduce array size by one
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return n;
+    }
+
 }
