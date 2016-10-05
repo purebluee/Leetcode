dcfd9950c18e4e9fa73b5475ae74170c57b1a0b7
Cong <c.wang049@gmail.com>
2016-10-04 22:34:02 -0400

    Q326

--- 326-PowerOfThree.java
+++ 326-PowerOfThree.java
@@ -1,19 +1,10 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
- //recursive solution
 public class Solution {
-    public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null)
-            return head;
-        ListNode newHead = reverseList(head.next);
-        head.next.next = head;
-        head.next = null;
-        return newHead;
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0)
+            return false;
+        while(n % 3 == 0){
+            n /= 3;
+        }
+        return n == 1;
     }
 }
