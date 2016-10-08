e9b34d9a95b8ceefde84d92fa1f32600e7fc1bc1
Cong <c.wang049@gmail.com>
2016-10-07 23:19:23 -0400

    update 198

--- 198-HouseRobber.java
+++ 198-HouseRobber.java
@@ -1,4 +1,22 @@
 public class Solution {
+    //DP with O(n) space:
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        if (nums.length == 1)
+            return nums[0];
+        int max = 0;
+        int len = nums.length;
+        int[] maxM = new int[len];
+        maxM[0] = nums[0];
+        maxM[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < len; i++){
+            maxM[i] = Math.max((maxM[i - 2] + nums[i]), maxM[i - 1]);
+        }
+        return maxM[len - 1];
+    }
+    //DP with constant space
     public int rob(int[] nums) {
         if (nums.length == 0)
             return 0;
