4d789e95f41293c5a068cf423f53fb76c22f068d
Cong <c.wang049@gmail.com>
2016-10-10 11:55:40 -0400

    update 205

--- 205-IsomorphicStrings.java
+++ 205-IsomorphicStrings.java
@@ -2,11 +2,12 @@ public class Solution {
     public boolean isIsomorphic(String s, String t) {
         if (s == null || t == null) return false;
         if (s.length() != t.length())   return false;
+        if (s == null || t == null) return false;
         HashMap<Character, Character> hm = new HashMap<>();
         
         for (int i = 0; i < s.length(); i++){
             if (hm.containsKey(s.charAt(i))){
-                if (hm.get(s.charAt(i)) ==t.charAt(i)){
+                if (hm.get(s.charAt(i)) == t.charAt(i)){
                     continue;
                 }else{
                     return false;
