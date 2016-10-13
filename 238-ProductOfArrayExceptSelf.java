2d86b0e471e94291825918eb8347d64724ff0b73
Cong <c.wang049@gmail.com>
2016-10-13 16:10:00 -0400

    edits

--- 238-ProductOfArrayExceptSelf.java
+++ 238-ProductOfArrayExceptSelf.java
@@ -1,16 +1,16 @@
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
+        int len = nums.length;
+        int[] result = new int[len];
         
-        result[n-1] = 1;
-        for(int i=n-2; i>=0; --i) {
-            result[i] = result[i+1] * nums[i+1];
+        result[len - 1] = 1;
+        for(int i = len - 2; i >= 0; i--) {
+            result[i] = result[i + 1] * nums[i + 1];
         }
         
-        for(int i=0; i<n-1; ++i) {
-            result[i] = result[i] * result[n-1];
-            result[n-1] = result[n-1] * nums[i];
+        for(int i=0; i<len-1; i++) {
+            result[i] = result[i] * result[len - 1];
+            result[len - 1] = result[len - 1] * nums[i];
         }
         
         return result;
