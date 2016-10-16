631c49747fd047262526cd18743f48be5d25cd39
C <cong@Cs-MacBook-Pro-2.local>
2016-10-16 17:03:28 -0400

    Q153

--- 153-FindMinimumInRotatedSortedArray.java
+++ 153-FindMinimumInRotatedSortedArray.java
@@ -15,6 +15,17 @@ As you see in the examples above, if we have case 1, we just return the leftmost
 Following is the code that implements the concept described above.
 */
 
+
+/*
+
+    (1) loop is left < right, which means inside the loop, left always < right
+    (2 ) since we use round up for mid, and left < right from (1), right would never be the same as mid
+    (3) Therefore, we compare mid with right, since they will never be the same from (2)
+    (4) if nums[mid] < nums[right], we will know the minimum should be in the left part, so we are moving right.
+        We can always make right = mid while we don't have to worry the loop will not ends. Since from (2), we know right would never be the same as mid, making right = mid will assure the interval is shrinking.
+    (5) if nums[mid] > nums[right], minimum should be in the right part, so we are moving left. Since nums[mid] > nums[right],mid can't be the minimum, we can safely move left to mid + 1, which also assure the interval is shrinking
+
+*/
 public class Solution {
     public int findMin(int[] nums) {
         if (nums == null)   return 0;
@@ -28,10 +39,10 @@ public class Solution {
             }
             //(nums[mid] > nums[start] && nums[mid] > nums[end])
             else{
-                start = mid + 1;
+                start = mid;
             } 
         }
-        return nums[start];
+        return nums[end];
     }
     
 }
