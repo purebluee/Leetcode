a9f4b835092d2e31df46ff65f684c258c6f99bdb
C <cong@Cs-MacBook-Pro-2.local>
2016-10-31 14:05:47 -0400

    updates

--- 139-WordBreak.java
+++ 139-WordBreak.java
@@ -14,3 +14,19 @@ public class Solution {
         return breakable[s.length()];
     }
 }
+
+public class Solution {
+    public boolean wordBreak(String s, Set<String> wordDict) {
+        if (s.length() == 0 || wordDict == null || wordDict.size() == 0){
+            return false;
+        }
+        boolean[] breakable = new boolean[s.length()+1];
+        breakable[0] = true;
+        for (int i = 1; i <= s.length(); i++){
+            for (int j = 0; j <= i && !breakable[i]; j++){
+                breakable[i] = wordDict.contains(s.substring(j, i)) && breakable[j];
+            }
+        }
+        return breakable[s.length()];
+    }
+}
--- 187-RepeatedDNASequences.java
+++ 187-RepeatedDNASequences.java
@@ -1,15 +1,18 @@
 public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        Set<String> seen = new HashSet<>(), repeated = new HashSet<>();
-        if (s.length() == 0 || s == null){
-            return new ArrayList(repeated);
+        Set<String> res = new HashSet<>();
+        Set<String> set = new HashSet<>();//must use hashset, not list! subseq might be the same!
+        if (s.length() < 10){
+            return new ArrayList<>(res);
         }
-        for (int i = 0; i < s.length() - 9; i++){
-            String ten = s.substring(i, i+10);
-            if (!seen.add(ten)){
-                repeated.add(ten);
+        for (int i = 0; i < s.length() - 9; i++){//length - 9  substring excludes last element!
+            String seq = s.substring(i, i + 10);
+            if (!set.contains(seq)){
+                set.add(seq);
+            }else{
+                res.add(seq);
             }
         }
-        return new ArrayList<>(repeated);
+        return new ArrayList<>(res);
     }
 }
