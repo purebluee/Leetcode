3d72f249746040cea0fa5b760d1772ba0532cf9a
C <cong@Cs-MacBook-Pro-2.local>
2016-11-14 20:51:10 -0500

    delete

--- Java/223-RectangleArea.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        int total = (C-A)*(D-B) + (G-E)*(H-F);
-        if ((B >= H) || (E >= C) || (A >= G) || (F >= D))   return total;
-        int dx = Math.min(C, G) - Math.max(A, E);
-        int dy = Math.min(D, H) - Math.max(B, F);
-        return total - dx * dy;
-    }
-}
--- Java/28-ImplementStrStr.java
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public int strStr(String haystack, String needle) {
-        if (needle == null || haystack == null || haystack.length() < needle.length()) return -1;
-        for (int i = 0; i <= haystack.length() - needle.length(); ++i){
-            if (haystack.substring(i, i+needle.length()).equals(needle))    return i;
-        }
-        return -1;
-    }
-}
