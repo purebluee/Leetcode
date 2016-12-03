9513df2ac10065b3e6ae01570a51d404e01def80
C <cong@Cs-MacBook-Pro-2.local>
2016-12-02 22:37:21 -0500

    update

--- Java/104-MaximumDepthOfBinaryTree.java
+++ Java/104-MaximumDepthOfBinaryTree.java
@@ -17,3 +17,49 @@ public class Solution {
         
     }
 }
+
+//Iterative:
+//BFS:
+public int maxDepth(TreeNode root) {
+    if(root == null) {
+        return 0;
+    }
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    int count = 0;
+    while(!queue.isEmpty()) {
+        int size = queue.size();
+        while(size-- > 0) {
+            TreeNode node = queue.poll();
+            if(node.left != null) {
+                queue.offer(node.left);
+            }
+            if(node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        count++;
+    }
+    return count;
+}
+//DFS:
+public int maxDepth(TreeNode root) {
+    Stack<TreeNode> nodes = new Stack<TreeNode>();
+    Stack<Integer> depths= new Stack<Integer>();
+    nodes.push(root);
+    depths.push(1);
+    int max = 0;
+    
+    while(!nodes.isEmpty()) {
+        TreeNode node = nodes.pop();
+        int depth = depths.pop();
+        if (node == null) continue;
+        if (depth > max) max = depth;
+        nodes.push(node.left);
+        depths.push(depth+1);
+        nodes.push(node.right);
+        depths.push(depth+1);
+    }
+    
+    return max;
+}
--- Java/136-SingleNumber.java
+++ Java/136-SingleNumber.java
@@ -14,3 +14,17 @@ public class Solution {
         return set.iterator().next();
     }
 }
+
+//XOR:
+public class Solution {
+    public int singleNumber(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        int res = 0;
+        for (int i = 0; i < nums.length; i++){
+            res = res ^ nums[i];
+        }
+        return res;
+    }
+}
